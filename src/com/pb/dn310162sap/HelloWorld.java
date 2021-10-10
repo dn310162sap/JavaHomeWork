4643c7065f9e7af4041cee3300f66340bfa6d61c
dn310162sap <92268899+dn310162sap@users.noreply.github.com>
2021-10-10 20:50:08 +0300

    Delete HelloWorld.java

--- src/com/pb/dn310162sap/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package com.pb.dn310162sap;
-
-import java.sql.SQLOutput;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
